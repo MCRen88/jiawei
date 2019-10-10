e1b616588f25edf82e0369924a1684a0ce0351ca
许妙纯 <xumiaochun@nidayedeMacBook-Pro.local>
2019-10-10 19:52:44 +0800

    minor:

--- explore_ts.py
+++ explore_ts.py
@@ -24,12 +24,13 @@ FIGURE_SIZE = (16, 7)
 
 def run():
     root_dir = get_user_data_dir()
-    original_path = join(root_dir, "706_dnm_tmp_3ZFwT#sum_iUserNum_300#20190620_16Days_valid6D_pipeline_test_input_node_test_data.csv")
+    original_path = join(root_dir, "706_dnm_tmp_3ZFwT#sum_iUserNum_300#20190620_16Days_valid6D_pipeline_test_input_node_train_data.csv")
+
     df = pd.read_csv(original_path)
     print(df.head())
     df["timestamp"] = df["timestamp"].map(millisec_to_str)
 
-    pic_path = join("/Users/stellazhao/research_space/jiawei", "tmp/pic/")
+    pic_path = join("/Users/xumiaochun/jiawei", "tmp/pic/")
     
     line_id_list = np.unique(df.line_id)
 
