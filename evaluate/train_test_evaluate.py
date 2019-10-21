fd17268d01630979fe8188a8da07aa97a66554d2
许妙纯 <xumiaochun@nidayedeMacBook-Pro.local>
2019-10-21 17:05:04 +0800

    minor:

--- evaluate/train_test_evaluate.py
+++ evaluate/train_test_evaluate.py
@@ -2,8 +2,6 @@
 
 from sklearn.metrics import classification_report,confusion_matrix,f1_score,average_precision_score
 
-
-
 print("\n\nlabels_train.anomaly, y_pred_train\n", (classification_report(labels_train.anomaly, y_pred_train)))
 print("\n\nlabels_test.anomaly, y_pred_test\n", (classification_report(labels_test.anomaly, y_pred_test)))
 
@@ -11,4 +9,3 @@ confusion_train = confusion_matrix(labels_train.anomaly, y_pred_train).ravel()
 print("confusion_train2", confusion_train)
 confusion_test = confusion_matrix(labels_test.anomaly, y_pred_test).ravel()
 print("confusion_test2", confusion_test)
-Precision_Recall_Curve2(labels_train.anomaly, anomaly_score_train)
